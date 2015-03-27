7e522214dfae2d1309028bb1700f000503251d3b
Russell Bryant <rbryant@redhat.com>
2015-03-27 13:04:09 -0400

    devstack: install and run ovs+ovn
    This patch updates the devstack integration to install ovs+ovn from
    source.  It creates the ovs, ovn, and ovn-nb databases and runs
    ovsdb-server and ovs-vswitchd.  Once ovn-nbd and ovn-controller exist,
    they will be run, as well.
    
    This also includes some other assorted changes made while I was
    getting this to work.
    
    Change-Id: I8eb4f88aed2101f60382fb8307265154066dc482

--- devstack/README.rst
+++ devstack/README.rst
@@ -2,13 +2,26 @@
  Enabling in Devstack
 ======================
 
-1. Download DevStack
+1. Download devstack and networking-ovn
 
-2. Add this repo as an external repository::
+     git clone http://git.openstack.org/openstack-dev/devstack.git
+     git clone http://git.openstack.org/stackforge/networking-ovn.git
 
-     > cat local.conf
-     [[local|localrc]]
-     enable_plugin networking-ovn http://git.openstack.org/stackforge/networking-ovn
-     enable_service ovn
+2. Add networking-ovn to devstack.  The minimal set of critical local.conf
+   additions are the following::
 
-3. run ``stack.sh``
+     cd devstack
+     cat << EOF >> local.conf
+     > enable_plugin networking-ovn http://git.openstack.org/stackforge/networking-ovn
+     > enable_service ovn
+     > EOF
+
+You can also use the provided example local.conf, or look at its contents to add
+to your own::
+
+     cd devstack
+     cp ../networking-ovn/devstack/local.conf.sample local.conf
+
+3. run devstack::
+
+     ./stack.sh
--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -11,7 +11,6 @@
 
 # ``stack.sh`` calls the entry points in this order:
 #
-# - is_ovn_enabled
 # - install_ovn
 # - configure_ovn
 # - configure_ovn_plugin
@@ -28,15 +27,15 @@ set +o xtrace
 # Defaults
 # --------
 
+# The git repo to use
+OVN_REPO=${OVN_REPO:-http://github.com/openvswitch/ovs.git}
+
+# The branch to use from $OVN_REPO
+OVN_BRANCH=${OVN_BRANCH:-origin/ovn}
+
+
 # Entry Points
 # ------------
-
-# Test if OVN is enabled
-# is_ovn_enabled
-function is_ovn_enabled {
-    [[ ,${ENABLED_SERVICES} =~ ,"ovn" ]] && return 0
-    return 1
-}
 
 # cleanup_ovn() - Remove residual data files, anything left over from previous
 # runs that a clean run would need to clean up
@@ -47,21 +46,45 @@ function cleanup_ovn {
 # configure_ovn() - Set config files, create data dirs, etc
 function configure_ovn {
     echo "Configuring OVN"
-
-    # Configure OVN
+    :
 }
 
 function configure_ovn_plugin {
     echo "Configuring Neutron for OVN"
 
-    # OVN plugin configuration information goes here
+    # TODO: The changes in setup.cfg should have this installed automatically
+    # but doesn't seem to be working
+    sudo cp $DEST/networking-ovn/etc/ovn.filters /etc/neutron/rootwrap.d/.
+
+    # ovs is installed with a prefix of /usr/local, so neutron-rootwrap needs to
+    # be allowed to find the tools installed in /usr/local/bin.
+    iniset /etc/neutron/rootwrap.conf DEFAULT exec_dirs \
+        "$(iniget /etc/neutron/rootwrap.conf DEFAULT exec_dirs),/usr/local/bin"
 }
 
 # init_ovn() - Initialize databases, etc.
 function init_ovn {
     # clean up from previous (possibly aborted) runs
     # create required data files
-    :
+
+    # Assumption: this is a dedicated test system and there is nothing important
+    # in the ovn, ovn-nb, or ovs databases.  We're going to trash them and
+    # create new ones on each devstack run.
+
+    base_dir=$DATA_DIR/ovs
+    mkdir -p $base_dir
+
+    for db in conf.db ovn.db ovnnb.db ; do
+        if [ -f $base_dir/$db ] ; then
+            rm -f $base_dir/$db
+        fi
+    done
+    rm -f $base_dir/*.db.~lock~
+
+    echo "Creating OVS, OVN, and OVN-Northbound Databases"
+    ovsdb-tool create $base_dir/ovn.db $DEST/ovs/ovn/ovn.ovsschema
+    ovsdb-tool create $base_dir/ovnnb.db $DEST/ovs/ovn/ovn-nb.ovsschema
+    ovsdb-tool create $base_dir/conf.db $DEST/ovs/vswitchd/vswitch.ovsschema
 }
 
 # install_ovn() - Collect source and prepare
@@ -69,19 +92,62 @@ function install_ovn {
     local _pwd=$(pwd)
     echo "Installing OVN and dependent packages"
 
-    # Do some awesome stuff to install OVN
+    # If OVS is already installed, remove it, because we're about to re-install
+    # it from source.
+    for package in openvswitch openvswitch-switch ; do
+        if is_package_installed $package ; then
+            uninstall_package $package
+        fi
+    done
+
+    cd $DEST/networking-ovn
+    sudo pip install .
+    cd ..
+
+    REPO_BASE="$(basename $OVN_REPO | cut -f1 -d'.')"
+    if [ ! -d $REPO_BASE ] ; then
+        git clone $OVN_REPO
+        cd $REPO_BASE
+        git checkout $OVN_BRANCH
+    else
+        cd $REPO_BASE
+    fi
+
+    # TODO: Can you create package list files like you can inside devstack?
+    install_package autoconf automake libtool gcc patch
+
+    if [ ! -f configure ] ; then
+        ./boot.sh
+    fi
+    ./configure
+    make -j$[$(nproc) + 1]
+    sudo make install
+    sudo chown $(whoami) /usr/local/var/run/openvswitch
+
+    cd $_pwd
 }
 
 # start_ovn() - Start running processes, including screen
 function start_ovn {
-    echo "Starting OPVN"
+    echo "Starting OVN"
 
-    # Start OVN up
+    local _pwd=$(pwd)
+    cd $DATA_DIR/ovs
+
+    ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
+                 --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
+                 --pidfile --detach conf.db ovn.db ovnnb.db
+    ovs-vsctl --no-wait init
+
+    sudo ovs-vswitchd --pidfile --detach
+
+    cd $_pwd
 }
 
 # stop_ovn() - Stop running processes (non-screen)
 function stop_ovn {
-    stop_process ovn
+    sudo killall ovsdb-server
+    sudo killall ovs-vswitchd
 }
 
 # main loop
@@ -92,10 +158,11 @@ if is_service_enabled ovn; then
         fi
         configure_ovn
         init_ovn
+        # We have to start at install time, because Neutron's post-config
+        # phase runs ovs-vsctl.
+        start_ovn
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
         configure_ovn_plugin
-        # This has to start before Neutron
-        start_ovn
 
         if is_service_enabled nova; then
             create_nova_conf_neutron
