5a4e353c5845e4dc1cdbeeee1ac81052db38237e
Kyle Mestery <mestery@mestery.com>
2015-10-09 17:35:14 +0000

    Explicitly install tox
    When testing OVN with Kuryr using a setup which only installs Neutron,
    keystone, mysql and OVN, the devstack run fails because tox is not
    installed. It was being pulled in when installing tempest. Since
    networking-ovn uses genconfig to generate a config file, make sure
    we install this ourselves.
    
    With this run, my devstack with Kuryr works now.
    
    Change-Id: I67afce78152f250920ef43334e7ee87440bcd075
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -190,6 +190,9 @@ function install_ovn {
     # TODO: Can you create package list files like you can inside devstack?
     install_package autoconf automake libtool gcc patch make
 
+    # Install tox, used to generate the config
+    pip_install tox
+
     if [ ! -f configure ] ; then
         ./boot.sh
     fi
