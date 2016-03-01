9a825cf73d3e838409515aa3fb627243593c96ef
Gary Kotton <gkotton@vmware.com>
2016-03-01 04:50:17 -0800

    OVN: : make use of neutron_lib constants
    Commit 87a79256c494c36f2d9597313f430b24c0110161 added neutron_lib
    for shared constants. This patch moves us to make use of the
    aforementioned library.
    
    NOTE: networking_ovn/common/utils.py is not updated as this
    constant does not existing in the neutron lib yet.
    
    Change-Id: Ib4586851df1fc4b68725886db4b1476893e87891

--- networking_ovn/plugin.py
+++ networking_ovn/plugin.py
@@ -16,6 +16,7 @@ import collections
 import netaddr
 import six
 
+from neutron_lib import constants as const
 from oslo_config import cfg
 from oslo_log import log
 from oslo_utils import importutils
@@ -34,7 +35,6 @@ from neutron.api.v2 import attributes as attr
 from neutron.callbacks import events
 from neutron.callbacks import registry
 from neutron.callbacks import resources
-from neutron.common import constants as const
 from neutron.common import exceptions as n_exc
 from neutron.common import rpc as n_rpc
 from neutron.common import topics
