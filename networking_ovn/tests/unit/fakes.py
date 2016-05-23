3d619f24800afd9f7b5f7ccc935ebd646ed501cd
Richard Theis <rtheis@us.ibm.com>
2016-05-23 06:35:09 -0500

    Add driver unit tests for ML2
    Add mechanism driver unit tests for ML2 based on the current
    L2 unit tests for the core plugin.
    
    Change-Id: Ibf2834c07d99be5b2ea80865f92c1a20a78cd05e
    Partial-Bug: #1578198

--- networking_ovn/tests/unit/fakes.py
+++ networking_ovn/tests/unit/fakes.py
@@ -12,7 +12,10 @@
 #    under the License.
 #
 
+import copy
 import mock
+import six
+import uuid
 
 
 class FakeOvsdbOvnIdl(object):
@@ -42,3 +45,211 @@ class FakeOvsdbOvnIdl(object):
         self.idl = mock.Mock()
         self.add_static_route = mock.Mock()
         self.delete_static_route = mock.Mock()
+
+
+class FakeResource(object):
+
+    def __init__(self, manager=None, info=None, loaded=False, methods=None):
+        """Set attributes and methods for a resource.
+
+        :param manager:
+            The resource manager
+        :param Dictionary info:
+            A dictionary with all attributes
+        :param bool loaded:
+            True if the resource is loaded in memory
+        :param Dictionary methods:
+            A dictionary with all methods
+        """
+        info = info or {}
+        methods = methods or {}
+
+        self.__name__ = type(self).__name__
+        self.manager = manager
+        self._info = info
+        self._add_details(info)
+        self._add_methods(methods)
+        self._loaded = loaded
+
+    def _add_details(self, info):
+        for (k, v) in six.iteritems(info):
+            setattr(self, k, v)
+
+    def _add_methods(self, methods):
+        """Fake methods with MagicMock objects.
+
+        For each <@key, @value> pairs in methods, add an callable MagicMock
+        object named @key as an attribute, and set the mock's return_value to
+        @value. When users access the attribute with (), @value will be
+        returned, which looks like a function call.
+        """
+        for (name, ret) in six.iteritems(methods):
+            method = mock.MagicMock(return_value=ret)
+            setattr(self, name, method)
+
+    def __repr__(self):
+        reprkeys = sorted(k for k in self.__dict__.keys() if k[0] != '_' and
+                          k != 'manager')
+        info = ", ".join("%s=%s" % (k, getattr(self, k)) for k in reprkeys)
+        return "<%s %s>" % (self.__class__.__name__, info)
+
+    def keys(self):
+        return self._info.keys()
+
+    def info(self):
+        return self._info
+
+
+class FakePort(object):
+    """Fake one or more ports."""
+
+    @staticmethod
+    def create_one_port(attrs=None):
+        """Create a fake port.
+
+        :param Dictionary attrs:
+            A dictionary with all attributes
+        :return:
+            A FakeResource object faking the port
+        """
+        attrs = attrs or {}
+
+        # Set default attributes.
+        fake_uuid = uuid.uuid4().hex
+        port_attrs = {
+            'admin_state_up': True,
+            'allowed_address_pairs': [{}],
+            'binding:host_id': 'binding-host-id-' + fake_uuid,
+            'binding:profile': {},
+            'binding:vif_details': {},
+            'binding:vif_type': 'ovs',
+            'binding:vnic_type': 'normal',
+            'device_id': 'device-id-' + fake_uuid,
+            'device_owner': 'compute:nova',
+            'dns_assignment': [{}],
+            'dns_name': 'dns-name-' + fake_uuid,
+            'extra_dhcp_opts': [{}],
+            'fixed_ips': [{'subnet_id': 'subnet-id-' + fake_uuid,
+                           'ip_address': '10.10.10.20'}],
+            'id': 'port-id-' + fake_uuid,
+            'mac_address': 'fa:16:3e:a9:4e:72',
+            'name': 'port-name-' + fake_uuid,
+            'network_id': 'network-id-' + fake_uuid,
+            'port_security_enabled': True,
+            'security_groups': [],
+            'status': 'ACTIVE',
+            'tenant_id': 'project-id-' + fake_uuid,
+        }
+
+        # Overwrite default attributes.
+        port_attrs.update(attrs)
+
+        return FakeResource(info=copy.deepcopy(port_attrs),
+                            loaded=True)
+
+
+class FakeSecurityGroup(object):
+    """Fake one or more security groups."""
+
+    @staticmethod
+    def create_one_security_group(attrs=None):
+        """Create a fake security group.
+
+        :param Dictionary attrs:
+            A dictionary with all attributes
+        :return:
+            A FakeResource object faking the security group
+        """
+        attrs = attrs or {}
+
+        # Set default attributes.
+        fake_uuid = uuid.uuid4().hex
+        security_group_attrs = {
+            'id': 'security-group-id-' + fake_uuid,
+            'name': 'security-group-name-' + fake_uuid,
+            'description': 'security-group-description-' + fake_uuid,
+            'tenant_id': 'project-id-' + fake_uuid,
+            'security_group_rules': [],
+        }
+
+        # Overwrite default attributes.
+        security_group_attrs.update(attrs)
+
+        return FakeResource(info=copy.deepcopy(security_group_attrs),
+                            loaded=True)
+
+
+class FakeSecurityGroupRule(object):
+    """Fake one or more security group rules."""
+
+    @staticmethod
+    def create_one_security_group_rule(attrs=None):
+        """Create a fake security group rule.
+
+        :param Dictionary attrs:
+            A dictionary with all attributes
+        :return:
+            A FakeResource object faking the security group rule
+        """
+        attrs = attrs or {}
+
+        # Set default attributes.
+        fake_uuid = uuid.uuid4().hex
+        security_group_rule_attrs = {
+            'direction': 'ingress',
+            'ethertype': 'IPv4',
+            'id': 'security-group-rule-id-' + fake_uuid,
+            'port_range_max': 22,
+            'port_range_min': 22,
+            'protocol': 'tcp',
+            'remote_group_id': None,
+            'remote_ip_prefix': '0.0.0.0/0',
+            'security_group_id': 'security-group-id-' + fake_uuid,
+            'tenant_id': 'project-id-' + fake_uuid,
+        }
+
+        # Overwrite default attributes.
+        security_group_rule_attrs.update(attrs)
+
+        return FakeResource(info=copy.deepcopy(security_group_rule_attrs),
+                            loaded=True)
+
+
+class FakeSubnet(object):
+    """Fake one or more subnets."""
+
+    @staticmethod
+    def create_one_subnet(attrs=None):
+        """Create a fake subnet.
+
+        :param Dictionary attrs:
+            A dictionary with all attributes
+        :return:
+            A FakeResource object faking the subnet
+        """
+        attrs = attrs or {}
+
+        # Set default attributes.
+        fake_uuid = uuid.uuid4().hex
+        subnet_attrs = {
+            'id': 'subnet-id-' + fake_uuid,
+            'name': 'subnet-name-' + fake_uuid,
+            'network_id': 'network-id-' + fake_uuid,
+            'cidr': '10.10.10.0/24',
+            'tenant_id': 'project-id-' + fake_uuid,
+            'enable_dhcp': True,
+            'dns_nameservers': [],
+            'allocation_pools': [],
+            'host_routes': [],
+            'ip_version': 4,
+            'gateway_ip': '10.10.10.1',
+            'ipv6_address_mode': 'None',
+            'ipv6_ra_mode': 'None',
+            'subnetpool_id': None,
+        }
+
+        # Overwrite default attributes.
+        subnet_attrs.update(attrs)
+
+        return FakeResource(info=copy.deepcopy(subnet_attrs),
+                            loaded=True)
--- networking_ovn/tests/unit/ml2/test_mech_driver.py
+++ networking_ovn/tests/unit/ml2/test_mech_driver.py
@@ -13,24 +13,242 @@
 #
 
 import mock
+from webob import exc
 
+from neutron.extensions import portbindings
+from neutron import manager
+from neutron.plugins.ml2 import config
+from neutron.tests import tools
 from neutron.tests.unit.plugins.ml2 import test_ext_portsecurity
 from neutron.tests.unit.plugins.ml2 import test_plugin
 
-from networking_ovn.ml2 import mech_driver
+from networking_ovn.common import acl as ovn_acl
+from networking_ovn.common import constants as ovn_const
+from networking_ovn.ovsdb import impl_idl_ovn
 from networking_ovn.tests.unit import fakes
 
 
 class TestOVNMechanismDriver(test_plugin.Ml2PluginV2TestCase):
 
-    def setUp(self):
-        super(TestOVNMechanismDriver, self).setUp()
-        self.plugin = mock.Mock()
-        self.driver = mech_driver.OVNMechanismDriver()
-        self.driver._ovn = fakes.FakeOvsdbOvnIdl()
+    _mechanism_drivers = ['logger', 'ovn']
+    _extension_drivers = ['port_security']
 
-    # TODO(rtheis): Need to add Fakes for context in order to test
-    # the OVNMechanismDriver methods.
+    def setUp(self):
+        impl_idl_ovn.OvsdbOvnIdl = fakes.FakeOvsdbOvnIdl()
+        config.cfg.CONF.set_override('extension_drivers',
+                                     self._extension_drivers,
+                                     group='ml2')
+        super(TestOVNMechanismDriver, self).setUp()
+        mm = manager.NeutronManager.get_plugin().mechanism_manager
+        self.mech_driver = mm.mech_drivers['ovn'].obj
+        self.mech_driver.initialize()
+        self.mech_driver._ovn = fakes.FakeOvsdbOvnIdl()
+
+        self.fake_subnet = fakes.FakeSubnet.create_one_subnet().info()
+        self.fake_port_no_sg = fakes.FakePort.create_one_port().info()
+
+        self.fake_sg_rule = \
+            fakes.FakeSecurityGroupRule.create_one_security_group_rule().info()
+        self.fake_sg = fakes.FakeSecurityGroup.create_one_security_group(
+            attrs={'security_group_rules': [self.fake_sg_rule]}
+        ).info()
+        self.fake_port_sg = fakes.FakePort.create_one_port(
+            attrs={'security_groups': [self.fake_sg['id']],
+                   'fixed_ips': [{'subnet_id': self.fake_subnet['id'],
+                                  'ip_address': '10.10.10.20'}]}
+        ).info()
+
+        self.sg_cache = {self.fake_sg['id']: self.fake_sg}
+        self.sg_ports_cache = {}
+        self.subnet_cache = {self.fake_subnet['id']: self.fake_subnet}
+
+    def test_add_acls_no_sec_group(self):
+        acls = ovn_acl.add_acls(self.mech_driver._plugin,
+                                mock.Mock(),
+                                self.fake_port_no_sg,
+                                {}, {}, {})
+        self.assertEqual([], acls)
+
+    def test_add_acls_with_sec_group(self):
+        expected_acls = []
+        expected_acls += ovn_acl.drop_all_ip_traffic_for_port(
+            self.fake_port_sg)
+        expected_acls += ovn_acl.add_acl_dhcp(
+            self.fake_port_sg, self.fake_subnet)
+        sg_rule_acl = ovn_acl.add_sg_rule_acl_for_port(
+            self.fake_port_sg, self.fake_sg_rule,
+            'outport == "' + self.fake_port_sg['id'] + '" ' +
+            '&& ip4 && ip4.src == 0.0.0.0/0 ' +
+            '&& tcp && tcp.dst == 22')
+        expected_acls.append(sg_rule_acl)
+
+        # Test with caches
+        acls = ovn_acl.add_acls(self.mech_driver._plugin,
+                                mock.Mock(),
+                                self.fake_port_sg,
+                                self.sg_cache,
+                                self.sg_ports_cache,
+                                self.subnet_cache)
+        self.assertEqual(expected_acls, acls)
+
+        # Test without caches
+        with mock.patch('neutron.db.db_base_plugin_v2.'
+                        'NeutronDbPluginV2.get_subnet',
+                        return_value=self.fake_subnet), \
+            mock.patch('neutron.db.securitygroups_db.'
+                       'SecurityGroupDbMixin.get_security_group',
+                       return_value=self.fake_sg):
+
+            acls = ovn_acl.add_acls(self.mech_driver._plugin,
+                                    mock.Mock(),
+                                    self.fake_port_sg,
+                                    {}, {}, {})
+            self.assertEqual(expected_acls, acls)
+
+    def test_port_invalid_binding_profile(self):
+        invalid_binding_profiles = [
+            {'tag': 0,
+             'parent_name': 'fakename'},
+            {'tag': 1024},
+            {'tag': 1024, 'parent_name': 1024},
+            {'parent_name': 'test'},
+            {'tag': 'test'},
+            {'vtep_physical_switch': 'psw1'},
+            {'vtep_logical_switch': 'lsw1'},
+            {'vtep_physical_switch': 'psw1', 'vtep_logical_switch': 1234},
+            {'vtep_physical_switch': 1234, 'vtep_logical_switch': 'lsw1'},
+            {'vtep_physical_switch': 'psw1', 'vtep_logical_switch': 'lsw1',
+             'tag': 1024},
+            {'vtep_physical_switch': 'psw1', 'vtep_logical_switch': 'lsw1',
+             'parent_name': 'fakename'},
+            {'vtep_physical_switch': 'psw1', 'vtep_logical_switch': 'lsw1',
+             'tag': 1024, 'parent_name': 'fakename'},
+        ]
+        with self.network(set_context=True, tenant_id='test') as net1:
+            with self.subnet(network=net1) as subnet1:
+                # succeed without binding:profile
+                with self.port(subnet=subnet1,
+                               set_context=True, tenant_id='test'):
+                    pass
+                # fail with invalid binding profiles
+                for invalid_profile in invalid_binding_profiles:
+                    try:
+                        kwargs = {ovn_const.OVN_PORT_BINDING_PROFILE:
+                                  invalid_profile}
+                        with self.port(
+                                subnet=subnet1,
+                                expected_res_status=403,
+                                arg_list=(
+                                ovn_const.OVN_PORT_BINDING_PROFILE,),
+                                set_context=True, tenant_id='test',
+                                **kwargs):
+                            pass
+                    except exc.HTTPClientError:
+                        pass
+
+    def test_create_port_security(self):
+        kwargs = {'mac_address': '00:00:00:00:00:01',
+                  'fixed_ips': [{'ip_address': '10.0.0.2'},
+                                {'ip_address': '10.0.0.4'}]}
+        with self.network(set_context=True, tenant_id='test') as net1:
+            with self.subnet(network=net1) as subnet1:
+                with self.port(subnet=subnet1,
+                               arg_list=('mac_address', 'fixed_ips'),
+                               set_context=True, tenant_id='test',
+                               **kwargs) as port:
+                    self.assertTrue(
+                        self.mech_driver._ovn.create_lport.called)
+                    called_args_dict = (
+                        (self.mech_driver._ovn.create_lport
+                         ).call_args_list[0][1])
+                    self.assertEqual(['00:00:00:00:00:01 10.0.0.2 10.0.0.4'],
+                                     called_args_dict.get('port_security'))
+
+                    data = {'port': {'mac_address': '00:00:00:00:00:02'}}
+                    req = self.new_update_request(
+                        'ports',
+                        data, port['port']['id'])
+                    req.get_response(self.api)
+                    self.assertTrue(
+                        self.mech_driver._ovn.set_lport.called)
+                    called_args_dict = (
+                        (self.mech_driver._ovn.set_lport
+                         ).call_args_list[0][1])
+                    self.assertEqual(['00:00:00:00:00:02 10.0.0.2 10.0.0.4'],
+                                     called_args_dict.get('port_security'))
+
+    def test_create_port_with_disabled_security(self):
+        kwargs = {'port_security_enabled': False}
+        with self.network(set_context=True, tenant_id='test') as net1:
+            with self.subnet(network=net1) as subnet1:
+                with self.port(subnet=subnet1,
+                               arg_list=('port_security_enabled',),
+                               set_context=True, tenant_id='test',
+                               **kwargs) as port:
+                    self.assertTrue(
+                        self.mech_driver._ovn.create_lport.called)
+                    called_args_dict = (
+                        (self.mech_driver._ovn.create_lport
+                         ).call_args_list[0][1])
+                    self.assertEqual([],
+                                     called_args_dict.get('port_security'))
+
+                    data = {'port': {'mac_address': '00:00:00:00:00:01'}}
+                    req = self.new_update_request(
+                        'ports',
+                        data, port['port']['id'])
+                    req.get_response(self.api)
+                    self.assertTrue(
+                        self.mech_driver._ovn.set_lport.called)
+                    called_args_dict = (
+                        (self.mech_driver._ovn.set_lport
+                         ).call_args_list[0][1])
+                    self.assertEqual([],
+                                     called_args_dict.get('port_security'))
+
+    def test_create_port_security_allowed_address_pairs(self):
+        kwargs = {'allowed_address_pairs':
+                  [{"ip_address": "1.1.1.1"},
+                   {"ip_address": "2.2.2.2",
+                    "mac_address": "22:22:22:22:22:22"}]}
+        with self.network(set_context=True, tenant_id='test') as net1:
+            with self.subnet(network=net1) as subnet1:
+                with self.port(subnet=subnet1,
+                               arg_list=('allowed_address_pairs',),
+                               set_context=True, tenant_id='test',
+                               **kwargs) as port:
+                    self.assertTrue(
+                        self.mech_driver._ovn.create_lport.called)
+                    called_args_dict = (
+                        (self.mech_driver._ovn.create_lport
+                         ).call_args_list[0][1])
+                    self.assertEqual(
+                        tools.UnorderedList(
+                            ["22:22:22:22:22:22 2.2.2.2",
+                             port['port']['mac_address'] + ' ' + '10.0.0.2'
+                             + ' ' + '1.1.1.1']),
+                        called_args_dict.get('port_security'))
+
+                    old_mac = port['port']['mac_address']
+
+                    # we are updating only the port mac address. So the
+                    # mac address of the allowed address pair ip 1.1.1.1
+                    # will have old mac address
+                    data = {'port': {'mac_address': '00:00:00:00:00:01'}}
+                    req = self.new_update_request(
+                        'ports',
+                        data, port['port']['id'])
+                    req.get_response(self.api)
+                    self.assertTrue(
+                        self.mech_driver._ovn.set_lport.called)
+                    called_args_dict = (
+                        (self.mech_driver._ovn.set_lport
+                         ).call_args_list[0][1])
+                    self.assertEqual(tools.UnorderedList(
+                        ["22:22:22:22:22:22 2.2.2.2",
+                         "00:00:00:00:00:01 10.0.0.2",
+                         old_mac + " 1.1.1.1"]),
+                        called_args_dict.get('port_security'))
 
 
 class OVNMechanismDriverTestCase(test_plugin.Ml2PluginV2TestCase):
@@ -38,10 +256,11 @@ class OVNMechanismDriverTestCase(test_plugin.Ml2PluginV2TestCase):
 
     def setUp(self):
         super(OVNMechanismDriverTestCase, self).setUp()
-        self.port_create_status = 'DOWN'
-        self.mech = mech_driver.OVNMechanismDriver
-        self.mech._ovn = fakes.FakeOvsdbOvnIdl()
-        self.mech._insert_port_provisioning_block = mock.Mock()
+        mm = manager.NeutronManager.get_plugin().mechanism_manager
+        self.mech_driver = mm.mech_drivers['ovn'].obj
+        self.mech_driver._ovn = fakes.FakeOvsdbOvnIdl()
+        self.mech_driver._insert_port_provisioning_block = mock.Mock()
+        self.mech_driver.vif_type = portbindings.VIF_TYPE_OVS
 
 
 class TestOVNMechansimDriverBasicGet(test_plugin.TestMl2BasicGet,
@@ -74,20 +293,18 @@ class TestOVNMechansimDriverSubnetsV2(test_plugin.TestMl2SubnetsV2,
 class TestOVNMechansimDriverPortsV2(test_plugin.TestMl2PortsV2,
                                     OVNMechanismDriverTestCase):
 
-    # TODO(rtheis): Debug test case failure.
+    # NOTE(rtheis): Override this test to verify that updating
+    # a port MAC fails when the port is bound.
     def test_update_port_mac(self):
-        pass
+        self.check_update_port_mac(
+            host_arg={portbindings.HOST_ID: 'fake-host'},
+            arg_list=(portbindings.HOST_ID,),
+            expected_status=exc.HTTPConflict.code,
+            expected_error='PortBound')
 
-    # TODO(rtheis): Debug test case failure.
-    def test_create_port_tolerates_db_deadlock(self):
-        pass
-
-    # TODO(rtheis): Debug test case failure.
+    # TODO(rtheis): Remove once [1] is fixed.
+    # [1] https://review.openstack.org/#/c/310682/
     def test_create_router_port_and_fail_create_postcommit(self):
-        pass
-
-    # TODO(rtheis): Debug test case failure.
-    def test_dhcp_provisioning_blocks_inserted_on_update(self):
         pass
 
 
