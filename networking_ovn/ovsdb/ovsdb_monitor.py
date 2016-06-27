5f95a06cd5c91e131c2ec60bda3e1d0d4bc10388
Richard Theis <rtheis@us.ibm.com>
2016-06-27 10:56:00 -0500

    Fix py34 unit test failures
    This patch set fixes py34 test failures in preparation for a new
    gate-networking-ovn-python34 job [1].
    
    [1] https://review.openstack.org/#/c/334585/
    
    Change-Id: I059172e0fe3993cb23887e48a6b471b9ad8d156d
    Partial-Bug: #1596090

--- networking_ovn/ovsdb/ovsdb_monitor.py
+++ networking_ovn/ovsdb/ovsdb_monitor.py
@@ -14,8 +14,8 @@
 
 import atexit
 from eventlet import greenthread
-import Queue
 import retrying
+from six.moves.queue import Queue
 import threading
 
 from oslo_log import log
@@ -137,7 +137,7 @@ class OvnDbNotifyHandler(object):
         self.driver = driver
         self.__watched_events = set()
         self.__lock = threading.Lock()
-        self.notifications = Queue.Queue()
+        self.notifications = Queue()
         self.notify_thread = greenthread.spawn_n(self.notify_loop)
         atexit.register(self.shutdown)
 
