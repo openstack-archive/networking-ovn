5c530c809aad9c4f1e1267549b606fb79fdd833c
Andreas Jaeger <aj@suse.com>
2016-10-14 07:41:10 +0200

    Fix tox_install.sh
    Translation sync is broken with a recent change to tox_install.sh, see
    http://logs.openstack.org/periodic/networking-ovn-propose-translation-update/7c27fe1/
    
    Import changes for this file from neutron-vpnaas to allow to run
    functional tests against neutron: We need to install the proper version
    of neutron when testing neutron changes with functional tests from
    this repo.
    Otherwise those tests install neutron HEAD.
    
    Use temporary directory for neutron install, add it to pep8 exclude list.
    
    Conflicts:
    	tools/tox_install.sh
    
    Change-Id: Ia260ba0e9d8142f0eb63a0c85a273f80c118855b
    (cherry picked from commit 07998fc4c7d0f89b00a045bae1379add39c4db3d)

--- tools/tox_install.sh
+++ tools/tox_install.sh
@@ -13,28 +13,41 @@
 # default:
 # pip install {opts} {packages}
 
-set -ex
-
 ZUUL_CLONER=/usr/zuul-env/bin/zuul-cloner
-neutron_installed=$(echo "import neutron" | python 2>/dev/null ; echo $?)
 BRANCH_NAME=stable/newton
+neutron_installed=$(echo "import neutron" | python 2>/dev/null ; echo $?)
+NEUTRON_DIR=$HOME/neutron
 
-cwd=$PWD
+set -e
+set -x
 
-CONSTRAINTS_FILE=$1
+install_cmd="pip install -c$1"
 shift
 
-install_cmd="pip install"
-if [ $CONSTRAINTS_FILE != "unconstrained" ]; then
-    install_cmd="$install_cmd -c$CONSTRAINTS_FILE"
-fi
-
-if [ $neutron_installed -eq 0 ]; then
+# The devstack based functional tests have neutron checked out in
+# $NEUTRON_DIR on the test systems - with the change to test in it.
+# Use this directory if it exists, so that this script installs the
+# neutron version to test here.
+# Note that the functional tests use sudo to run tox and thus
+# variables used for zuul-cloner to check out the correct version are
+# lost.
+if [ -d "$NEUTRON_DIR" ]; then
+    echo "FOUND Neutron code at $NEUTRON_DIR - using"
+    $install_cmd -U -e $NEUTRON_DIR
+elif [ $neutron_installed -eq 0 ]; then
     echo "ALREADY INSTALLED" > /tmp/tox_install.txt
+    location=$(python -c "import neutron; print(neutron.__file__)")
+    echo "ALREADY INSTALLED at $location"
+
     echo "Neutron already installed; using existing package"
 elif [ -x "$ZUUL_CLONER" ]; then
     echo "ZUUL CLONER" > /tmp/tox_install.txt
-    cd /tmp
+    # Make this relative to current working directory so that
+    # git clean can remove it. We cannot remove the directory directly
+    # since it is referenced after $install_cmd -e.
+    mkdir -p .tmp
+    NEUTRON_DIR=$(/bin/mktemp -d -p $(pwd)/.tmp)
+    pushd $NEUTRON_DIR
     $ZUUL_CLONER --cache-dir \
         /opt/git \
         --branch $BRANCH_NAME \
@@ -42,10 +55,13 @@ elif [ -x "$ZUUL_CLONER" ]; then
         openstack/neutron
     cd openstack/neutron
     $install_cmd -e .
-    cd "$cwd"
+    popd
 else
     echo "PIP HARDCODE" > /tmp/tox_install.txt
-    $install_cmd -U -egit+https://git.openstack.org/openstack/neutron@$BRANCH_NAME#egg=neutron
+    if [ -z "$NEUTRON_PIP_LOCATION" ]; then
+        NEUTRON_PIP_LOCATION="git+https://git.openstack.org/openstack/neutron@$BRANCH_NAME#egg=neutron"
+    fi
+    $install_cmd -U -e ${NEUTRON_PIP_LOCATION}
 fi
 
 $install_cmd -U $*
