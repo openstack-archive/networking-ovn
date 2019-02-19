f7a234a6ff414cfa52591f39d1b59d3d7dcab6e2
Daniel Alvarez <dalvarez@redhat.com>
2019-02-19 14:44:36 +0000

    Support ports belonging to Neutron DHCP agents (ii)
    This patch is fixing a bug from [0] when there were
    network:dhcp owner ports belonging to Neutron DHCP agents.
    In this case, metadata port was not being created and
    instances were not able to reach the service.
    
    [0] ecef7c27c15a875f4b48440ea7c212984db88b62
    Related-Bug: #1804390
    Signed-off-by: Daniel Alvarez <dalvarez@redhat.com>
    
    Change-Id: I26edf7ba8f66d39f0c1e6f4fa60ed81cb243c6a3

--- networking_ovn/tests/functional/test_maintenance.py
+++ networking_ovn/tests/functional/test_maintenance.py
@@ -699,31 +699,31 @@ class TestMaintenance(_TestMaintenanceHelper):
         ovn_config.cfg.CONF.set_override('ovn_metadata_enabled', True,
                                          group='ovn')
         neutron_net = self._create_network('network1')
-        metadata_ports = self._ovn_client._get_metadata_ports(
+        metadata_port = self._ovn_client._find_metadata_port(
             self.context, neutron_net['id'])
 
         # Assert the metadata port exists
-        self.assertEqual(1, len(metadata_ports))
+        self.assertIsNotNone(metadata_port)
 
         # Delete the metadata port
-        self._delete('ports', metadata_ports[0]['id'])
+        self._delete('ports', metadata_port['id'])
 
-        metadata_ports = self._ovn_client._get_metadata_ports(
+        metadata_port = self._ovn_client._find_metadata_port(
             self.context, neutron_net['id'])
 
         # Assert the metadata port is gone
-        self.assertEqual(0, len(metadata_ports))
+        self.assertIsNone(metadata_port)
 
         # Call the maintenance thread to fix the problem, it will raise
         # NeverAgain so that the job only runs once at startup
         self.assertRaises(periodics.NeverAgain,
                           self.maint.check_metadata_ports)
 
-        metadata_ports = self._ovn_client._get_metadata_ports(
+        metadata_port = self._ovn_client._find_metadata_port(
             self.context, neutron_net['id'])
 
         # Assert the metadata port was re-created
-        self.assertEqual(1, len(metadata_ports))
+        self.assertIsNotNone(metadata_port)
 
     def test_check_metadata_ports_not_enabled(self):
         ovn_config.cfg.CONF.set_override('ovn_metadata_enabled', False,
