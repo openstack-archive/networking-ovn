6a89dbfe47ae1f384e08fede3bb6afae2ada7b9c
Lucas Alvares Gomes <lucasagomes@gmail.com>
2019-07-31 13:41:34 +0100

    Only set "unknown" in LSP that makes sense
    Prior to this patch, the "unknown" address was being set the logical
    switch ports solely based on whether the port security was enabled or
    disabled. That's not how it's intended to work.
    
    With this patch the "unknown" address is only set to the normal logical
    switch ports, those which types are "router", "localnet" or "localport"
    won't be affected.
    
    The maintenance task was updated to correct this behavior for existing
    ports (the maintenance was suppose to be removed in the T cycle, this
    patch bumps it to U cycle instead due to this change).
    
    Closes-Bug: #1838535
    Related-Bug: #1815270
    Change-Id: I3c01bd7d1685c8a7e13a55e545e98baf19e9a0f9
    Signed-off-by: Lucas Alvares Gomes <lucasagomes@gmail.com>

--- networking_ovn/common/maintenance.py
+++ networking_ovn/common/maintenance.py
@@ -403,7 +403,7 @@ class DBInconsistenciesPeriodics(object):
 
         raise periodics.NeverAgain()
 
-    # TODO(lucasagomes): Remove this in the T cycle
+    # TODO(lucasagomes): Remove this in the U cycle
     # A static spacing value is used here, but this method will only run
     # once per lock due to the use of periodics.NeverAgain().
     @periodics.periodic(spacing=600, run_immediately=True)
@@ -413,13 +413,22 @@ class DBInconsistenciesPeriodics(object):
             return
 
         for port in self._nb_idl.lsp_list().execute(check_error=True):
-            addresses = port.addresses
-            if not port.port_security and 'unknown' not in addresses:
-                addresses.append('unknown')
-            elif port.port_security and 'unknown' in addresses:
-                addresses.remove('unknown')
-            else:
+
+            if port.type == ovn_const.LSP_TYPE_LOCALNET:
                 continue
+
+            addresses = port.addresses
+            type_ = port.type.strip()
+            if not port.port_security:
+                if not type_ and ovn_const.UNKNOWN_ADDR not in addresses:
+                    addresses.append(ovn_const.UNKNOWN_ADDR)
+                elif type_ and ovn_const.UNKNOWN_ADDR in addresses:
+                    addresses.remove(ovn_const.UNKNOWN_ADDR)
+            else:
+                if type_ and ovn_const.UNKNOWN_ADDR in addresses:
+                    addresses.remove(ovn_const.UNKNOWN_ADDR)
+                elif not type_ and ovn_const.UNKNOWN_ADDR in addresses:
+                    addresses.remove(ovn_const.UNKNOWN_ADDR)
 
             self._nb_idl.lsp_set_addresses(
                 port.name, addresses=addresses).execute(check_error=True)
