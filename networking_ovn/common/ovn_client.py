0c90536af6e1cf0c55de32b5e31bbde77088e58b
Terry Wilson <twilson@redhat.com>
2021-09-14 10:13:20 -0500

    Fix neutron_pg_drop-related startup issues
    There is a python-ovs bug that allows transactions to be executed
    before it has started monitoring the db for changes. This breaks
    ovsdbapp's AddCommand behavior, which looks up a row in memory
    post-commit to return it to the caller, because we process the
    reply from ovsdb-server that has the UUID in it, but when we try to
    look it up in memory, it isn't there since that is handled via the
    monitor notifications.
    
    Since we don't care about the return value, we can just ignore the
    KeyError. This is a slightly different fix than the one in neutron
    but it does the same thing.
    
    Closes-Bug: #1938766
    Change-Id: I78af15970a86923fb810e903d38634b6e60a8fe4

--- networking_ovn/common/ovn_client.py
+++ networking_ovn/common/ovn_client.py
@@ -2051,7 +2051,18 @@ class OVNClient(object):
                 txn.add(self._nb_idl.pg_add_ports(pg_name, ports_ids))
 
     def _add_port_to_drop_port_group(self, port, txn):
-        self.create_default_drop_port_group()
+        try:
+            self.create_default_drop_port_group()
+        except KeyError:
+            # Due to a bug in python-ovs, we can send transactions before
+            # the initial OVSDB is populated in memory. This can break
+            # the AddCommand post_commit method which tries to return a
+            # row looked up by the newly commited row's uuid. Since we
+            # don't care about the return value from the PgAddCommand, we
+            # can just catch the KeyError and continue. This can be
+            # removed when the python-ovs bug is resolved.
+            pass
+
         txn.add(self._nb_idl.pg_add_ports(ovn_const.OVN_DROP_PORT_GROUP_NAME,
                 port))
 
